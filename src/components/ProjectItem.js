8d1cc2fe659e071d4b68b0b056713ed9388db83a
github username <github email address>
2021-07-20 17:38:27 -0400

    almost

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,16 @@
 import React from "react";
+import ProjectList from "./ProjectList";
+
 
 function ProjectItem({ name, about, technologies }) {
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{
+    
+        <span>{technologies}</span>
+      }</div>
     </div>
   );
 }
